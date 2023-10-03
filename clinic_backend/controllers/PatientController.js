92085d52df2453873dc15c751c4f671c6625aec8
Ibrahim <ibrahimaboras2@gmail.com>
2023-10-03 22:50:52 +0300

    Sign Up Patient (not tested on DB)

--- clinic_backend/controllers/PatientController.js
+++ clinic_backend/controllers/PatientController.js
@@ -5,9 +5,15 @@ const Patient = require('../models/Patient');
 const signUp = async(req, res) => {
     const {username, name, email, password, dob, gender, mobile_number, Efull_name, Emobile_number} = req.body
 
-    const patient = new Patient(username, name, email, password, dob, gender, mobile_number, Efull_name, Emobile_number)
+    const emergency_contact = {Efull_name, Emobile_number}
+
+    const patient = new Patient(username, name, email, password, dob, gender, mobile_number, emergency_contact)
 
     await patient.save()
 
     res.status(200).json(patient)
+}
+
+module.exports = {
+    signUp
 }
