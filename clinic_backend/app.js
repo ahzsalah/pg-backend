91446241d4f2ae4a0648728b3f3ce705b9759100
Ibrahim <ibrahimaboras2@gmail.com>
2023-10-05 14:28:56 +0300

    merge working

--- clinic_backend/app.js
+++ clinic_backend/app.js
@@ -69,22 +69,42 @@ app.use('/api/doctors', doctorsRoute);
 //   res.render('error');
 // });
 
-// connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db and Server is running on port', process.env.PORT)
-    })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
 
 // routes
 app.get('/', (req, res) => {
   res.send('Welcome back, user') 
 })
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connencted to DB & Listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
 // const PORT = 3000;
 // app.listen(process.env.PORT, () => {
